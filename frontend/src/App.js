4e900f61cedb3015220f447a877dcb6bcaa3101b
Pablo.ED <echiparradominguezp@gmail.com>
2026-05-28 18:01:16 -0500

    forzar URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
+const API_URL = 'https://js-examen-backend.onrender.com/api';
 
 // ==================== PÁGINA DE INICIO ====================
 function Inicio() {
